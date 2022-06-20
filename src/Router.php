82023ed40c15f1cc4d46a1f2444cad1a2089ba49
panteleyR <panteley98@bk.ru>
2022-06-20 13:05:04 +0300

    change

--- src/Router.php
+++ src/Router.php
@@ -4,27 +4,44 @@ declare(strict_types=1);
 
 namespace Lilith\Router;
 
+use Lilith\DependencyInjection\ContainerInterface;
 use Lilith\Http\Message\RequestInterface;
+use Lilith\Http\Message\ResponseInterface;
+use Lilith\Http\Parser\HttpRequestParser;
 
 class Router implements RouterInterface
 {
     protected array $routes = [];
 
-    public function findRoute(string|RequestInterface $request): array
+    public function execute(ContainerInterface $container, RequestInterface $request): ResponseInterface
     {
+        [$class, $method, $args] = $this->findRoute($request);
+
+        return $container->get($class)->{$method}($request, ...$args);
+    }
+
+    public function findRoute(RequestInterface $request): array
+    {
+        $uri = HttpRequestParser::parseUri($request);
+
         foreach ($this->routes as $route => $routeConfig) {
-            if (is_string($request) && $route !== $request) {
-                continue;
-            }
+            $routePattern = '/' . addcslashes(preg_replace(
+                '/\/:(\w+)/',
+                '/(\w+)',
+                $routeConfig
+            ), '/') . '/';
+            $foundCount = preg_match($routePattern, $uri->getPath(), $matches);
+            unset($matches[0]);
 
-            if ($routeConfig['uri'] !== $request->getUri()) {
-                continue;
-            }
+            if ($request->getMethod() === $routeConfig['method'] && $foundCount === 1) {
+                $route = explode('::', $routeConfig['handler']);
+                $route[] = $matches;
 
-            return explode('::', $routeConfig['handler']);
+                return $route;
+            }
         }
 
-        throw new \Exception('route not found');
+        throw new \Exception('Route not found');
     }
 
     public function setRoutes(array $routes): void
--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Lilith\Router;
 
+use Lilith\DependencyInjection\ContainerInterface;
 use Lilith\Http\Message\RequestInterface;
+use Lilith\Http\Message\ResponseInterface;
 
 interface RouterInterface
 {
-    public function findRoute(string|RequestInterface $request): array;
+    public function findRoute(RequestInterface $request): array;
+    public function execute(ContainerInterface $container, RequestInterface $request): ResponseInterface;
     public function setRoutes(array $routes): void;
 }
